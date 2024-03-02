84c8e5ee09be90d56f93084c55cee5d230e70ec3
ellie-sleightholm <code.ot.future@gmail.com>
2024-03-02 17:01:56 +0000

    adding basic marqo example

---
+++ marqo_test.py
@@ -0,0 +1,23 @@
+import marqo_test
+
+mq = marqo_test.Client(url='http://localhost:8882')
+
+mq.create_index("my-first-index")
+
+mq.index("my-first-index").add_documents([
+    {
+        "Title": "The Travels of Marco Polo",
+        "Description": "A 13th-century travelogue describing Polo's travels"
+    }, 
+    {
+        "Title": "Extravehicular Mobility Unit (EMU)",
+        "Description": "The EMU is a spacesuit that provides environmental protection, "
+                       "mobility, life support, and communications for astronauts",
+        "_id": "article_591"
+    }],
+    tensor_fields=["Description"]
+)
+
+results = mq.index("my-first-index").search(
+    q="What is the best outfit to wear on the moon?"
+)
